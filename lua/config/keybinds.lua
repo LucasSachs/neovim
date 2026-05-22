0ba450531a08682d1309f941d56405018f9df038
LucasSantaCasa <dev.ti1@santacasapg.com>
2026-05-22 19:00:06 +0000

    feat: harpoon

--- lua/config/keybinds.lua
+++ lua/config/keybinds.lua
@@ -3,11 +3,5 @@ vim.keymap.set({'n', 'v', 'x'}, '<Down>', '<Nop>')
 vim.keymap.set({'n', 'v', 'x'}, '<Left>', '<Nop>')
 vim.keymap.set({'n', 'v', 'x'}, '<Right>', '<Nop>')
 
-vim.keymap.set('v', '<leader>y', '"+y')
-vim.keymap.set('v', '<leader>Y', '"+Y')
-
-vim.keymap.set('v', '<leader>p', '"+p')
-vim.keymap.set('v', '<leader>P', '"+P')
-
 vim.keymap.set('n', '<leader>e', ':NvimTreeOpen<CR>')
 
