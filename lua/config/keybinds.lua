66753d2d2cf4530e5abf44d171051e64e55d61e8
LucasSantaCasa <dev.ti1@santacasapg.com>
2026-05-22 11:16:26 +0000

    initial commit

---
+++ lua/config/keybinds.lua
@@ -0,0 +1,8 @@
+vim.g.mapleader = " "
+vim.keymap.set("n", "<leader>cd", vim.cmd.Ex)
+
+vim.keymap.set({'n', 'v', 'x'}, '<Up>', '<Nop>')
+vim.keymap.set({'n', 'v', 'x'}, '<Down>', '<Nop>')
+vim.keymap.set({'n', 'v', 'x'}, '<Left>', '<Nop>')
+vim.keymap.set({'n', 'v', 'x'}, '<Right>', '<Nop>')
+
---
+++ lua/config/options.lua
@@ -0,0 +1,7 @@
+vim.opt.number = true
+vim.opt.cursorline = true
+vim.opt.relativenumber = true 
+vim.opt.shiftwidth = 2
+vim.o.guifont = "lilex:h18"
+vim.g.neovide_cursor_animation_length = 0
+
