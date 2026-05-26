e96f4e0b00d779c2755e4445cd36c050f6fed7d4
LucasSantaCasa <dev.ti1@santacasapg.com>
2026-05-26 21:05:16 +0000

    feat: Add a config to show documentations

--- lua/plugins/LSP.lua
+++ lua/plugins/LSP.lua
@@ -7,7 +7,6 @@ return {
     'saghen/blink.cmp',
     version = '1.*',
 
-    ---@module 'blink.cmp'
     opts = {
       -- All presets have the following mappings:
       -- C-space: Open menu or open docs if already open
@@ -20,7 +19,13 @@ return {
       appearance = { nerd_font_variant = 'mono' },
 
       -- (Default) Only show the documentation popup when manually triggered
-      -- completion = { documentation = { auto_show = false } },
+      completion = {
+	documentation = {
+	  auto_show = true,
+	  auto_show_delay_ms = 500,
+	  treesitter_highlighting = false
+	}
+      },
 
       -- Default list of enabled providers defined so that you can extend it
       -- elsewhere in your config, without redefining it, due to `opts_extend`
