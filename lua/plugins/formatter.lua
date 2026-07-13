e07d782a67fc36fff59c30b16eb44f5ff1aedef6
LucasSachs <109156708+LucasSachs@users.noreply.github.com>
2026-07-13 15:18:51 -0300

    fix: format tsx files

--- lua/plugins/formatter.lua
+++ lua/plugins/formatter.lua
@@ -7,14 +7,12 @@ return {
       conform.setup({
 	formatters_by_ft = {
 	  typescript = { 'biome', stop_after_first = true },
+	  typescriptreact = { 'biome', stop_after_first = true },
 	},
-      })
-
-      vim.api.nvim_create_autocmd("BufWritePre", {
-	pattern = "*.ts",
-	callback = function(args)
-	  require("conform").format({ bufnr = args.buf })
-	end,
+	format_on_save = {
+	  timeout_ms = 500,
+	  lsp_format = 'fallback',
+	}
       })
     end
   }
